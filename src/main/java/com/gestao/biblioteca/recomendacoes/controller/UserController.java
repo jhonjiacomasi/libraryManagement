3e14655f264c8cd0e4b7a58af39355ca9318288e
jhonjiacomasi <jhon.jiacomasi@gmail.com>
2024-10-13 11:01:46 -0300

    Criação do metodo de atualizar Usuarios por Id, ajuste controller e service

--- src/main/java/com/gestao/biblioteca/recomendacoes/controller/UserController.java
+++ src/main/java/com/gestao/biblioteca/recomendacoes/controller/UserController.java
@@ -6,9 +6,11 @@ import java.util.UUID;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,32 +19,54 @@ import com.gestao.biblioteca.recomendacoes.dto.UserDto;
 import com.gestao.biblioteca.recomendacoes.model.User;
 import com.gestao.biblioteca.recomendacoes.service.UserService;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-
 
 @RestController
 @RequestMapping("/v1")
 @RequiredArgsConstructor
 public class UserController {
-	
-	private final UserService userService ;
+
+	private final UserService userService;
 
 	@GetMapping("/allUsers")
 	public ResponseEntity<List<User>> getALLBooks() {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.getALLBooks());
 	}
-	
+
 	@GetMapping("/User/{id}")
 	public ResponseEntity<User> getUserById(@PathVariable UUID id) {
-	 Optional<User> user = userService.getUserByID(id);
+		Optional<User> user = userService.getUserByID(id);
 		return ResponseEntity.ok(user.get());
 	}
-	
+
 	@PostMapping("/saveUser")
 	public ResponseEntity<User> saveUser(@RequestBody UserDto userDto) {
 
 		return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(userDto));
 	}
 	
+	@PutMapping("/updateUser/{uuid}")
+	public ResponseEntity<User> updateUser(@PathVariable UUID uuid, @RequestBody @Valid UserDto userDto){
+		try {
+			User updateUser = userService.updateUser(uuid, userDto);
+			return ResponseEntity.ok(updateUser);
+			
+		} catch (EntityNotFoundException e) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+		}
+		catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+		}
+		
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable UUID id) {
+		userService.deleteUserById(id);
+		return ResponseEntity.status(HttpStatus.OK).body("User delete sucessfull");
+	}
+
 }
--- src/main/java/com/gestao/biblioteca/recomendacoes/service/UserService.java
+++ src/main/java/com/gestao/biblioteca/recomendacoes/service/UserService.java
@@ -10,6 +10,7 @@ import com.gestao.biblioteca.recomendacoes.dto.UserDto;
 import com.gestao.biblioteca.recomendacoes.model.User;
 import com.gestao.biblioteca.recomendacoes.repository.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -36,5 +37,29 @@ public class UserService {
 	public Optional<User> getUserByID(UUID uuid) {
 		return userRepository.findById(uuid);
 	}
+	
+	@Transactional
+	public User updateUser(UUID id, UserDto userDto) {
+		return userRepository.findById(id)
+  			   .map(existingUser-> updateExistingUser(existingUser,userDto))
+ 	          .orElseThrow(() -> new EntityNotFoundException("User not foun with ID: "+ id));
+	
+	}
+	
+	private User updateExistingUser(User existingUser,UserDto userDto) {
+		existingUser.setName(userDto.getName());
+		existingUser.setEmail(userDto.getEmail());
+		existingUser.setTelephone(userDto.getTelephone());
+		
+		return userRepository.save(existingUser);
+		
+	}
+	@Transactional
+	public void deleteUserById(UUID uuid) {
+		userRepository.deleteById(uuid);
+	}
 
+	
+	
+	
 }
